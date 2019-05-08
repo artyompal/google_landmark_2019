20ed4215ec542065d94e9d9f6de37b4f346bfe67
artyom <cppg@cppg.co.uk>
2019-05-08 20:28:09 +0100

    added some code into KNN implementation

--- knn.py
+++ knn.py
@@ -8,30 +8,33 @@ from glob import glob
 from typing import *
 
 import numpy as np
+import pandas as pd
 import faiss
 
+from sklearn.model_selection import train_test_split
 from scipy.stats import describe
 from tqdm import tqdm
 from debug import dprint
 
 K = 20
-RESULTS_DIR = '../predicts'
+KNN_PREDICTS_DIR = '../predicts'
 USE_GPU = True
 USE_COSINE_DIST = False
+DIMS = 2048
 
 def search_against_fragment(train_features: np.ndarray, test_features: np.ndarray) \
     -> Tuple[np.ndarray, np.ndarray]:
     if USE_GPU:
         # build a flat index (CPU)
         if USE_COSINE_DIST:
-            index_flat = faiss.IndexFlat(d, faiss.METRIC_INNER_PRODUCT)
+            index_flat = faiss.IndexFlat(DIMS, faiss.METRIC_INNER_PRODUCT)
         else:
-            index_flat = faiss.IndexFlatL2(d)
+            index_flat = faiss.IndexFlatL2(DIMS)
 
         # make it into a GPU index
         index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
     else:
-        index_flat = faiss.IndexFlatIP(d)
+        index_flat = faiss.IndexFlatIP(DIMS)
 
     index_flat.add(train_features)
     print("total size of index:", index_flat.ntotal)
@@ -74,6 +77,7 @@ def merge_results(index1: np.ndarray, distances1: np.ndarray, index2: np.ndarray
     dprint(describe(best_distances))
     return best_indices, best_distances
 
+'''
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print(f'usage: {sys.argv[0]} test_features.npy train_features_1.npy ...')
@@ -82,13 +86,13 @@ if __name__ == "__main__":
     test_fname = sys.argv[1]
     train_fnames = sys.argv[2:]
 
-    if not os.path.exists(RESULTS_DIR):
-        os.makedirs(RESULTS_DIR)
+    if not os.path.exists(KNN_PREDICTS_DIR):
+        os.makedirs(KNN_PREDICTS_DIR)
 
     model_name = os.path.splitext(os.path.basename(test_fname))[0]
     model_name = model_name[5:] if model_name.startswith('test_') else model_name
     model_name = model_name[:-7] if model_name.endswith('_part00') else model_name
-    result_fname = os.path.join(RESULTS_DIR, f'dist_{model_name}.npz')
+    result_fname = os.path.join(KNN_PREDICTS_DIR, f'dist_{model_name}.npz')
     print("will save results to", result_fname)
 
     test_features = np.squeeze(np.load(test_fname))
@@ -99,7 +103,6 @@ if __name__ == "__main__":
     print(test_features)
     print("first vector:")
     print("shape", test_features[0].shape, "non-zeros", np.count_nonzero(test_features[0]))
-    d = test_features[0].shape[0]
 
     if USE_GPU:
         print("initializing CUDA")
@@ -133,3 +136,128 @@ if __name__ == "__main__":
 
     print("writing results to", result_fname)
     np.savez(result_fname, indices=best_index, distances=best_distances)
+'''
+
+def load_features(filename: str) -> np.ndarray:
+    features = np.load(filename)
+    features = features.reshape(features.shape[0], -1)
+
+    if USE_COSINE_DIST:
+        features /= (np.linalg.norm(features, axis=1, keepdims=True) + 1e-8)
+
+    return features
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f'usage: {sys.argv[0]} test_features.npy train_features_1.npy ...')
+        sys.exit()
+
+    test_fname = sys.argv[1]
+    train_fnames = sys.argv[2:]
+
+    if not os.path.exists(KNN_PREDICTS_DIR):
+        os.makedirs(KNN_PREDICTS_DIR)
+
+    model_name = os.path.splitext(os.path.basename(test_fname))[0]
+    model_name = model_name[5:] if model_name.startswith('test_') else model_name
+    model_name = model_name[:-7] if model_name.endswith('_part00') else model_name
+    result_fname = os.path.join(KNN_PREDICTS_DIR, f'dist_{model_name}.npz')
+    print("will save results to", result_fname)
+
+
+    ''' Algorithm:
+    1. define level-2 train and validation sets
+    2. for every sample from validation set, find K nearest samples from the train set
+    3. make a prediction about classes
+    4. calculate the metric
+    5. take full train set
+    6. for every sample from the test set, find K nearest samples from the full train set
+    7. make a prediction
+    8. generate submission
+    '''
+
+    # 1. define level-2 train and validation sets
+
+    if len(sys.argv) < 3:
+        print(f'usage: {sys.argv[0]} test_features.npy train_features_1.npy ...')
+        sys.exit()
+
+    test_fname = sys.argv[1]
+    train_fnames = sys.argv[2:]
+
+    if not os.path.exists(KNN_PREDICTS_DIR):
+        os.makedirs(KNN_PREDICTS_DIR)
+
+    level1_val = pd.read_csv('../data/splits/50_samples_18425_classes_fold_0_val.csv')
+    orig_train_df = pd.read_csv('../data/train.csv')
+    dprint(orig_train_df.shape)
+
+    # reminder_train = pd.read_csv('../data/splits/50_samples_fold_0_train.csv')
+    # reminder_val = pd.read_csv('../data/splits/50_samples_fold_0_train.csv')
+    # level2_full_df = pd.concat([level1_val, reminder_train, reminder_val])
+
+    level2_full_df = orig_train_df.loc[~orig_train_df.id.isin(level1_val)]
+    train_df, val_df  = train_test_split(level2_full_df, shuffle=True, random_state=0)
+
+    dprint(level2_full_df.shape)
+    dprint(train_df.shape)
+    dprint(val_df.shape)
+
+    train_bitmask = orig_train_df.id.isin(train_df.id)
+    val_bitmask = orig_train_df.id.isin(val_df.id)
+    dprint(sum(train_bitmask))
+    dprint(sum(train_bitmask.shape))
+    dprint(sum(val_bitmask))
+    dprint(sum(val_bitmask.shape))
+
+
+    # 2. for every sample from validation set, find K nearest samples from the train set
+    # if USE_GPU:
+    #     print("initializing CUDA")
+    #     res = faiss.StandardGpuResources()
+
+    val_offset = 0
+    total_val_samples = 0
+
+    for val_fragment in tqdm(train_fnames):
+        val_features = load_features(val_fragment)
+        fragment_size = val_features.shape[0]
+        mask = val_bitmask[val_offset : val_offset + fragment_size]
+        dprint(val_features.shape)
+        dprint(sum(mask))
+
+        val_features = val_features[mask]
+        dprint(val_features.shape)
+
+        total_val_samples += val_features.shape[0]
+
+        # best_index, best_distance = None, None
+        # train_offset = 0
+        #
+        # for train_fragment in tqdm(train_fnames):
+        #     train_features = load_features(train_fragment)
+        #
+        #     idx, dist = search_against_fragment(train_features, val_features)
+        #     idx += train_offset
+        #     train_offset += train_features.shape[0]
+        #
+        #     if best_index is None:
+        #         best_index, best_distances = idx, dist
+        #     else:
+        #         best_index, best_distances = merge_results(best_index, best_distances, id)
+
+        val_offset += fragment_size
+
+    dprint(total_val_samples)
+
+    # 3. make a prediction about classes
+
+    # 4. calculate the metric
+
+    # 5. take full train set
+
+    # 6. for every sample from the test set, find K nearest samples from the full train set
+
+    # 7. make a prediction
+
+    # 8. generate submission
