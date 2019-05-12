a4fea362ad5634c0f804acb27380fb34891122d3
artyom <cppg@cppg.co.uk>
2019-05-12 21:47:05 +0100

    provisionally fixed the kernel

--- kernel.py
+++ kernel.py
@@ -1,37 +1,45 @@
-#!/usr/bin/python3.6
+#!/usr/bin/env python
 
 import numpy as np
 import pandas as pd
 import os
+import time
+
+from typing import Any, Optional, Tuple
+from zipfile import ZipFile
 
 from sklearn.preprocessing import LabelEncoder
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-import torch.optim as optim
+import torch.backends.cudnn as cudnn
 
-from torch.optim import lr_scheduler
 from torch.utils.data import TensorDataset, DataLoader, Dataset
 from PIL import Image
 from tqdm import tqdm
 
-from torch.utils.data.sampler import SubsetRandomSampler
-from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR
+IN_KERNEL = os.environ.get('KAGGLE_WORKING_DIR') is not None
+MIN_SAMPLES_PER_CLASS = 50
+BATCH_SIZE = 512
+LEARNING_RATE = 1e-2
+LR_STEP = 3
+LR_FACTOR = 0.2
+NUM_WORKERS = 1
+MAX_STEPS_PER_EPOCH = 100
+NUM_EPOCHS = 1
+LOG_FREQ = 50
+NUM_TOP_PREDICTS = 20
 
-
-class ImageDataset(torch.utils.data.Dataset):
-    def __init__(self, dataframe: pd.DataFrame, path: str, mode: str,
-                 image_size: int) -> None:
+class ImageDataset(torch.utils.data.Dataset[np.ndarray]):
+    def __init__(self, dataframe: pd.DataFrame, mode: str) -> None:
         print(f'creating data loader - {mode}')
         assert mode in ['train', 'val', 'test']
 
         self.df = dataframe
-        self.path = path
         self.mode = mode
-        self.image_size = image_size
+        self.zipfile = ZipFile(f'../input/{mode}.zip')
 
         self.transforms = transforms.Compose([
             transforms.ToTensor(),
@@ -43,12 +51,11 @@ class ImageDataset(torch.utils.data.Dataset):
         ''' Returns: tuple (sample, target) '''
         filename = self.df.id.values[index]
 
-        sample = Image.open(os.path.join(self.path, filename + '.jpg'))
+        sample = Image.open(self.zipfile.open(filename + '.jpg'))
         assert sample.mode == 'RGB'
-        image = np.array(sample)
 
+        image = np.array(sample)
         assert image.dtype == np.uint8
-        assert image.shape == (self.image_size, self.image_size, 3)
 
         image = self.transforms(image)
 
@@ -59,6 +66,30 @@ class ImageDataset(torch.utils.data.Dataset):
 
     def __len__(self) -> int:
         return self.df.shape[0]
+
+def GAP(predicts: torch.Tensor, confs: torch.Tensor, targets: torch.Tensor) -> float:
+    ''' Simplified GAP@1 metric: only one prediction per sample is supported '''
+    assert len(predicts.shape) == 1
+    assert len(confs.shape) == 1
+    assert len(targets.shape) == 1
+    assert predicts.shape == confs.shape and confs.shape == targets.shape
+
+    sorted_confs, indices = torch.sort(confs, descending=True)
+
+    confs = confs.cpu().numpy()
+    predicts = predicts[indices].cpu().numpy()
+    targets = targets[indices].cpu().numpy()
+
+    res, true_pos = 0.0, 0
+
+    for i, (c, p, t) in enumerate(zip(confs, predicts, targets)): # type: ignore
+        rel = int(p == t)
+        true_pos += rel
+
+        res += true_pos / (i + 1) * rel
+
+    res /= targets.shape[0] # FIXME: incorrect, not all test images depict landmarks
+    return res
 
 class AverageMeter(object):
     ''' Computes and stores the average and current value '''
@@ -77,84 +108,60 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
-
-
-def load_data(fold: int) -> Any:
+def load_data() -> Any:
     torch.multiprocessing.set_sharing_strategy('file_system')
     cudnn.benchmark = True
 
-    print('config:')
-    print(pprint.pformat(config))
+    df = pd.read_csv('../input/train.csv')
+    df.drop(columns='url', inplace=True)
 
-    fname = f'{config.data.train_filename}_fold_{fold}_'
-    train_df = pd.read_csv(os.path.join(config.data.data_dir, fname + 'train.csv'))
-    val_df = pd.read_csv(os.path.join(config.data.data_dir, fname + 'val.csv'))
-    print('train_df', train_df.shape, 'val_df', val_df.shape)
+    # only use classes which have at least MIN_SAMPLES_PER_CLASS samples
+    counts = df.landmark_id.value_counts()
+    selected_classes = counts[counts >= MIN_SAMPLES_PER_CLASS].index
+    num_classes = selected_classes.shape[0]
+    print('classes with at least N samples:', num_classes)
 
-    val_df = pd.concat([c[1].iloc[:config.val.images_per_class]
-                        for c in val_df.groupby('landmark_id')])
-    print('val_df after class filtering', val_df.shape)
+    train_df = df.loc[df.landmark_id.isin(selected_classes)]
+    print('train_df', train_df.shape)
 
-    test_df = pd.read_csv('../data/test.csv', dtype=str)
+    test_df = pd.read_csv('../input/test.csv', dtype=str)
     test_df.drop(columns='url', inplace=True)
     print('test_df', test_df.shape)
 
-    test_df = test_df.loc[test_df.id.apply(lambda img: os.path.exists(os.path.join(
-        config.data.test_dir, img + '.jpg')))]
+    # filter non-existing test images
+    with ZipFile('../input/test.zip') as zf:
+        image_list = set(zf.namelist())
+
+    test_df = test_df.loc[test_df.id.apply(lambda img: img + '.jpg' in image_list)]
     print('test_df after filtering', test_df.shape)
 
     label_encoder = LabelEncoder()
     label_encoder.fit(train_df.landmark_id.values)
     print('found classes', len(label_encoder.classes_))
-    assert len(label_encoder.classes_) == config.model.num_classes
+    assert len(label_encoder.classes_) == num_classes
 
     train_df.landmark_id = label_encoder.transform(train_df.landmark_id)
-    val_df.landmark_id = label_encoder.transform(val_df.landmark_id)
 
+    train_dataset = ImageDataset(train_df, mode='train')
+    test_dataset = ImageDataset(test_df, mode='test')
 
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE,
+                              shuffle=False, num_workers=NUM_WORKERS, drop_last=True)
 
-    train_dataset = ImageDataset(train_df, path=config.data.train_dir, mode='train',
-                                 image_size=config.model.image_size,
-                                 num_classes=config.model.num_classes,
-                                 images_per_class=config.train.images_per_class,
-                                 aug_type='albu', augmentor=transform_train)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
+                             shuffle=False, num_workers=NUM_WORKERS)
 
-    val_dataset = ImageDataset(val_df, path=config.data.train_dir, mode='val',
-                               image_size=config.model.image_size,
-                               num_classes=config.model.num_classes)
-
-    test_dataset = ImageDataset(test_df, path=config.data.test_dir, mode='test',
-                                image_size=config.model.image_size,
-                                num_classes=config.model.num_classes)
-
-    sampler = None
-    shuffle = config.train.shuffle
-
-    train_loader = DataLoader(train_dataset, batch_size=config.train.batch_size,
-                              sampler=sampler, shuffle=shuffle,
-                              num_workers=config.num_workers, drop_last=True)
-
-    val_loader = DataLoader(val_dataset, batch_size=config.val.batch_size,
-                            shuffle=False, num_workers=config.num_workers)
-
-    test_loader = DataLoader(test_dataset, batch_size=config.test.batch_size,
-                             shuffle=False, num_workers=config.num_workers)
-
-
-    return train_loader, val_loader, test_loader, label_encoder
+    return train_loader, test_loader, label_encoder, num_classes
 
 def train(train_loader: Any, model: Any, criterion: Any, optimizer: Any,
-          epoch: int, lr_scheduler: Any, lr_scheduler2: Any) -> None:
+          epoch: int, lr_scheduler: Any) -> None:
     print(f'epoch {epoch}')
     batch_time = AverageMeter()
     losses = AverageMeter()
     avg_score = AverageMeter()
 
     model.train()
-
-    num_steps = len(train_loader)
-    if config.train.max_steps_per_epoch is not None:
-        num_steps = min(len(train_loader), config.train.max_steps_per_epoch)
+    num_steps = min(len(train_loader), MAX_STEPS_PER_EPOCH)
 
     print(f'total batches: {num_steps}')
 
@@ -179,24 +186,11 @@ def train(train_loader: Any, model: Any, criterion: Any, optimizer: Any,
         loss.backward()
         optimizer.step()
 
-        lr_updated = False
-
-        if is_scheduler_continuous(config.scheduler.name):
-            lr_scheduler.step()
-            lr_updated = True
-
-        if is_scheduler_continuous(config.scheduler2.name):
-            lr_scheduler2.step()
-            lr_updated = True
-
-        if lr_updated:
-            lr_str = f'\tlr {get_lr(optimizer):.08f}'
-
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if i % config.train.log_freq == 0:
+        if i % LOG_FREQ == 0:
             print(f'{epoch} [{i}/{num_steps}]\t'
                         f'time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                         f'loss {losses.val:.4f} ({losses.avg:.4f})\t'
@@ -214,30 +208,16 @@ def inference(data_loader: Any, model: Any) -> Tuple[torch.Tensor, torch.Tensor,
     all_predicts, all_confs, all_targets = [], [], []
 
     with torch.no_grad():
-        for i, data in enumerate(tqdm(data_loader, disable=config.in_kernel)):
+        for i, data in enumerate(tqdm(data_loader, disable=IN_KERNEL)):
             if data_loader.dataset.mode != 'test':
                 input_, target = data
             else:
                 input_, target = data, None
 
-            if config.test.num_ttas != 1 and data_loader.dataset.mode == 'test':
-                bs, num_ttas, c, h, w = input_.size()
-                input_ = input_.view(-1, c, h, w)
+            output = model(input_.cuda())
+            output = activation(output)
 
-                output = model(input_.cuda())
-                output = activation(output)
-
-                if config.test.tta_combine_func == 'max':
-                    output = output.view(bs, num_ttas, -1).max(dim=1)[0]
-                elif config.test.tta_combine_func == 'mean':
-                    output = output.view(bs, num_ttas, -1).mean(dim=1)
-                else:
-                    assert False
-            else:
-                output = model(input_.cuda())
-                output = activation(output)
-
-            confs, predicts = torch.topk(output, config.test.num_predicts)
+            confs, predicts = torch.topk(output, NUM_TOP_PREDICTS)
             all_confs.append(confs)
             all_predicts.append(predicts)
 
@@ -250,9 +230,8 @@ def inference(data_loader: Any, model: Any) -> Tuple[torch.Tensor, torch.Tensor,
 
     return predicts, confs, targets
 
-def generate_submission(val_loader: Any, test_loader: Any, model: Any,
-                        label_encoder: Any, epoch: int, model_path: Any) -> np.ndarray:
-    sample_sub = pd.read_csv('../data/recognition_sample_submission.csv')
+def generate_submission(test_loader: Any, model: Any, label_encoder: Any) -> np.ndarray:
+    sample_sub = pd.read_csv('../input/recognition_sample_submission.csv')
 
     predicts_gpu, confs_gpu, _ = inference(test_loader, model)
     predicts, confs = predicts_gpu.cpu().numpy(), confs_gpu.cpu().numpy()
@@ -264,7 +243,7 @@ def generate_submission(val_loader: Any, test_loader: Any, model: Any,
     print(np.array(confs))
 
     sub = test_loader.dataset.df
-    def concat(label, conf):
+    def concat(label: np.ndarray, conf: np.ndarray) -> str:
         return ' '.join([f'{L} {c}' for L, c in zip(label, conf)])
     sub['landmarks'] = [concat(label, conf) for label, conf in zip(labels, confs)]
 
@@ -275,13 +254,22 @@ def generate_submission(val_loader: Any, test_loader: Any, model: Any,
     sample_sub.to_csv('submission.csv')
 
 if __name__ == '__main__':
+    train_loader, test_loader, label_encoder, num_classes = load_data()
 
-    model_conv = torchvision.models.resnet50(pretrained=True)
-    model_conv.avg_pool = nn.AvgPool2d((5,10))
-    model_conv.last_linear = nn.Linear(model_conv.last_linear.in_features, 5005)
+    model = torchvision.models.resnet50(pretrained=True)
+    model.avg_pool = nn.AvgPool2d((5,10))
+    model.last_linear = nn.Linear(model.last_linear.in_features, num_classes)
 
-    criterion = nn.BCEWithLogitsLoss()
+    criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(model_conv.parameters(), lr=0.01)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP,
+                                                   gamma=LR_FACTOR)
 
+    for epoch in range(1, NUM_EPOCHS + 1):
+        print('-' * 50)
+        train(train_loader, model, criterion, optimizer, epoch, lr_scheduler)
+        lr_scheduler.step()
+
+    print('inference mode')
+    generate_submission(test_loader, model, label_encoder)
